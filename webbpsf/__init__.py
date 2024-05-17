9285dc5eb594a2959c99b811d7dc058099404149
Bradley Sappington <bsappington@stsci.edu>
2024-05-17 15:38:00 -0400

    revert __init__.py

--- webbpsf/__init__.py
+++ webbpsf/__init__.py
@@ -14,53 +14,26 @@ Documentation can be found online at https://webbpsf.readthedocs.io/
 """
 
 import sys
-
 from astropy import config as _config
-from poppy import (
-    display_ee,
-    display_profiles,
-    display_psf,
-    display_psf_difference,
-    fwcentroid,
-    measure_centroid,
-    measure_ee,
-    measure_fwhm,
-    measure_radial,
-    measure_sharpness,
-    radial_profile,
-    specFromSpectralType,
-)
-
-from . import trending, utils
-from .match_data import setup_sim_to_match_file
-from .opds import enable_adjustable_ote
-from .roman import WFI, RomanCoronagraph
-from .utils import measure_strehl, restart_logging, setup_logging, system_diagnostic
-from .webbpsf_core import (
-    FGS,
-    MIRI,
-    NIRISS,
-    JWInstrument,
-    NIRCam,
-    NIRSpec,
-    SpaceTelescopeInstrument,
-    instrument,
-)
 
 try:
     from .version import __version__
 except ImportError:
-    __version__ = ''
+    __version__ = ""
 
-__minimum_python_version__ = '3.9'
+__minimum_python_version__ = "3.9"
 
 
 class UnsupportedPythonError(Exception):
     pass
 
 
-if sys.version_info < tuple((int(val) for val in __minimum_python_version__.split('.'))):
-    raise UnsupportedPythonError('webbpsf does not support Python < {}'.format(__minimum_python_version__))
+if sys.version_info < tuple(
+    (int(val) for val in __minimum_python_version__.split("."))
+):
+    raise UnsupportedPythonError(
+        "webbpsf does not support Python < {}".format(__minimum_python_version__)
+    )
 
 
 # This tuple gives the *minimum* version of the WebbPSF data package
@@ -77,40 +50,82 @@ class Conf(_config.ConfigNamespace):
 
     default_oversampling = _config.ConfigItem(
         4,
-        'Default '
-        + 'oversampling factor: number of times more finely sampled than '
-        + 'an integer pixel for the grid spacing in the PSF calculation.',
+        "Default "
+        + "oversampling factor: number of times more finely sampled than "
+        + "an integer pixel for the grid spacing in the PSF calculation.",
     )
 
     default_output_mode = _config.ConfigItem(
-        'both',
-        "Should output include the oversampled PSF, a copy rebinned onto the integer detector spacing, or both? \
-            Options: 'oversampled','detector','both' ",
+        "both",
+        "Should output include the oversampled PSF, a copy rebinned onto the integer detector spacing, or both?\
+        Options: 'oversampled','detector','both' ",
     )
-    default_fov_arcsec = _config.ConfigItem(5.0, 'Default field of view size, in arcseconds per side of the square ')
+    default_fov_arcsec = _config.ConfigItem(
+        5.0, "Default field of view size, in arcseconds per side of the square "
+    )
 
     # Should be package settings:
     WEBBPSF_PATH = _config.ConfigItem(
-        'from_environment_variable',
-        'Directory path to data files required for WebbPSF calculations, such as OPDs and filter transmissions. \
-            This will be overridden by the environment variable $WEBBPSF_PATH, if present.',
+        "from_environment_variable",
+        "Directory path to data files required for WebbPSF calculations, such as OPDs and filter transmissions.\
+        This will be overridden by the environment variable $WEBBPSF_PATH, if present.",
     )
     autoconfigure_logging = _config.ConfigItem(
         False,
-        'Should WebbPSF configure logging for itself and POPPY? This adds handlers that report '
-        'calculation progress and information',
+        "Should WebbPSF configure logging for itself and POPPY? This adds handlers that report "
+        "calculation progress and information",
     )
     logging_level = _config.ConfigItem(
-        ['INFO', 'DEBUG', 'WARN', 'ERROR', 'CRITICAL', 'NONE'],
+        ["INFO", "DEBUG", "WARN", "ERROR", "CRITICAL", "NONE"],
         # (the default value is the first item in the options list)
-        'Desired logging level for WebbPSF optical calculations.',
+        "Desired logging level for WebbPSF optical calculations.",
     )
-    logging_filename = _config.ConfigItem('none', 'Desired filename to save log messages to.')
-    logging_format_screen = _config.ConfigItem('[%(name)7s] %(message)s', 'Format for lines logged to the screen.')
+    logging_filename = _config.ConfigItem(
+        "none", "Desired filename to save log messages to."
+    )
+    logging_format_screen = _config.ConfigItem(
+        "[%(name)7s] %(message)s", "Format for lines logged to the screen."
+    )
     logging_format_file = _config.ConfigItem(
-        '%(asctime)s [%(name)s:%(levelname)s] %(filename)s:%(lineno)d: %(message)s',
-        'Format for lines logged to a file.',
+        "%(asctime)s [%(name)s:%(levelname)s] %(filename)s:%(lineno)d: %(message)s",
+        "Format for lines logged to a file.",
     )
 
 
 conf = Conf()
+
+from . import utils
+from . import trending
+from .utils import setup_logging, restart_logging, system_diagnostic, measure_strehl
+
+from poppy import (
+    display_psf,
+    display_psf_difference,
+    display_ee,
+    measure_ee,
+    display_profiles,
+    radial_profile,
+    measure_radial,
+    measure_fwhm,
+    measure_sharpness,
+    measure_centroid,
+    specFromSpectralType,
+    fwcentroid,
+)
+
+from .webbpsf_core import (
+    instrument,
+    SpaceTelescopeInstrument,
+    JWInstrument,
+    NIRCam,
+    NIRISS,
+    NIRSpec,
+    MIRI,
+    FGS,
+)
+
+from .opds import enable_adjustable_ote
+
+from .roman import WFI, RomanCoronagraph
+
+from .match_data import setup_sim_to_match_file
