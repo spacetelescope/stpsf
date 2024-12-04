fa0e860468523c56fc2f7239deee1cb4895d1a4f
Marshall Perrin <mperrin@stsci.edu>
2024-12-04 17:11:59 -0500

    Avoid adding detector IPC to MIRI MRS IFU sims. Improve testing rigor & consistency

--- stpsf/stpsf_core.py
+++ stpsf/stpsf_core.py
@@ -1147,6 +1147,19 @@ class JWInstrument(SpaceTelescopeInstrument):
         except KeyError:
             raise ValueError('Not a valid aperture name for {}: {}'.format(self.name, aperture_name))
 
+    def _get_pixelscale_from_apername(self, apername):
+        """Simple utility function to look up pixelscale from apername"""
+        ap = self.siaf[apername]
+        # Here we make the simplifying assumption of **square** pixels, which is true within 0.5%.
+        # The slight departures from this are handled in the distortion model; see distortion.py
+        return (ap.XSciScale + ap.YSciScale) / 2
+
+    @property
+    def mode(self):
+        # This exists just for API consistency with the subclasses that have an imaging vs IFU mode toggle,
+        # so that all JWST instrument classes have a mode attribute, consistently, whether used or not
+        return "imaging"
+
     def _get_fits_header(self, result, options):
         """populate FITS Header keywords"""
         super(JWInstrument, self)._get_fits_header(result, options)
@@ -1252,7 +1265,9 @@ class JWInstrument(SpaceTelescopeInstrument):
         add_distortion = options.get('add_distortion', True)
         crop_psf = options.get('crop_psf', True)
         # you can turn on/off IPC corrections via the add_ipc option, default True.
-        add_ipc = options.get('add_ipc', True)
+        # except for IFU mode simulations, it doesn't make sense to add the regular detector IPC
+        # instead the more complex IFU broadening models should be applied
+        add_ipc = options.get('add_ipc', True if self.mode != 'IFU' else False)
 
         # Add distortion if set in calc_psf
         if add_distortion:
